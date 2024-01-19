585473bbe20baeb06d7024a64cd2fabd4e2c6aa4
Chae Kim <chaekim0820@gmail.com>
2024-01-19 11:27:02 -0500

    end of lecture

--- Lecture_Examples/Jan19.rkt
+++ Lecture_Examples/Jan19.rkt
@@ -26,3 +26,33 @@
         (if (zero? b)
             a
             (gcd b(remainder a b)))))
+
+; mylength returns the length of a list
+(define mylength
+    (lambda (lis)
+        (if (null? lis)
+            0
+            (+ 1 (mylength( cdr lis))))))
+
+; determine if an atom is in a list of atoms
+(define member?
+    (lambda (x lis)
+        (if (null? lis)
+            #f
+            (if (eq? x (car lis)) ;eq? is the same as == in java
+                #t
+                (member? x (cdr lis)))))) 
+    ;;; Below is an alternative way to do it
+    ;;; (cond
+    ;;;     [(null? lis) #f]
+    ;;;     [(eq? x (car lis)) #t
+    ;;;     [else (member? x (cdr lis))])))
+
+; numoccuring takes an atom and a list and returns the number of times 
+; the atom appears in the list
+(define numoccuring
+    (lambda (x lis)
+        (cond
+            [(null? lis)    0]
+            [(eq? x (car lis)) (+ 1 (numoccurring x (cdr lis)))]
+            [else (numoccurring x (cdr lis))])))

feb33fd1cf59ed04ade1a6915df21c8c07f49156
Chae Kim <chaekim0820@gmail.com>
2024-01-31 15:08:58 -0500

    Working on 10

--- Homeworks/cxk445-Scheme-HW.rkt
+++ Homeworks/cxk445-Scheme-HW.rkt
@@ -102,30 +102,65 @@
       ((> (car lis) (maxvalue* (cdr lis))) (car lis)) ; compare if two numbers; car bigger
       (else (maxvalue* (cdr lis)))))) ; recursed value bigger
 
-;; helper) helper function for number 10; returns true if the next atom equals x, the input atom
+;; helper) helper function for moveXleft*; returns true if the next atom equals x, the input atom
 (define search
   (lambda (x lis)
     (cond
       ((null? lis) #f)
-      ((list? (car lis)) (or (search x (car lis)) (search x (cdr lis))))
-      ((equal? (car lis) x) #t)
-      (else (search x (cdr lis))))))
+      ((list? (car lis)) (or (search x (car lis)) (search x (cdr lis)))) ; car is list
+      ((equal? (car lis) x) #t) ; car equals x
+      (else (search x (cdr lis)))))) ; car does not equal x
 
 ;; 10) takes an atom and a list with sublists; returns the same except
 ;;     the first occurrence of the atom X in the list shifted to left
-(define moveXleft* ; works for all X, not just first
-  (lambda (x lis) ; FIX
+(define moveXleft*
+  (lambda (x lis)
     (cond
       ((null? lis) '())
-      ((equal? (car lis) x) (cdr lis))
-      ;; List containing x
-      ((and (list? (car lis)) (search x (car lis))) (cons (moveXleft* x (car lis)) (cdr lis)))
-      ;; Current element is list & next is x
-      ((and (list? (car lis)) (and (pair? (cdr lis)) (equal? x (car (cdr lis)))))
-       (cons (myappend (moveXleft* x (car lis)) (cons x '())) (moveXleft* x (cdr lis))))
-      ;; Current atom & next is x
-      ((and (pair? (cdr lis)) (equal? (car (cdr lis)) x)) (cons x (cons (car lis) (cdr (cdr lis)))))
-      ;; x first in the list
-      ((and (and (pair? (cdr lis)) (pair? (car (cdr lis)))) (equal? (car (car (cdr lis))) x))
-       (myappend (cons (car lis) (cons x '())) (moveXleft* x (cdr lis)))) 
-      (else (cons (car lis) (moveXleft* x (cdr lis)))))))
+      ((equal? (car lis) x) (cdr lis)) ; car is x; get rid of it
+      ((and (pair? (car lis)) (search x (car lis))) ; In car's sublist, there's an x
+       (cons (moveXleft* x (car lis)) (cdr lis)))
+      ((and (pair? (car lis)) (equal? x (car (cdr lis)))) ; currently a list & bring x in
+       (cons (myappend (car lis) (cons x '())) (moveXleft* x (cdr lis)))) ; add x at the end of current list
+      ((and (not (list? (car lis))) (equal? x (cdr (car lis)))) ; car/cdr not list & cdr==x
+       (cons x (cons (car lis) (moveXleft* x (cdr lis))))) ; swap x and car
+      ((and (pair? (cdr lis)) (equal? x (car (car (cdr lis))))) ; cdr is a list containing x first
+       (cons (car lis) (cons x (moveXleft* x (cdr lis))))) ; add x after car
+      (else (cons (car lis) (moveXleft* x (cdr lis))))))) ; Else, just keep recursing
+
+
+;;;; **********************************************************************
+;;;; Outline) broadly 3 categories for moveXleft*
+;;;; Still return '() when lis null
+;;;; When x encountered, get rid of it
+;;;; Use a helper function to see if a car sublist contains x
+;;;;     => only first occurrence removed; recurse on that & append cdr
+;;;; 1) out => in (currently a list & bring x in); create a new list & append? (cdr is x & car is list)
+;;;; 2) stays in/out; cdr is x & car is not a list; no list involved => do similar to moveXleft
+;;;; 3) in => out; have to look into list; add to after car (myappend)
+;;;;    Special cases: (((x)) vs. (x)); 1st sublist with x in front
+;;;;        Look exactly if a sublist starts with x; just another if statement
+;;;;            if car is a list & its first element is a sublist with x in the front,
+;;;;            make the first element x & add the sublist next
+;;;; If nothing, just keep recursing
+;;;; ***********************************************************************
+
+
+
+
+;(define moveXleft* ; works for all X, not just first
+;  (lambda (x lis) 
+;    (cond
+;      ((null? lis) '())
+;      ((equal? (car lis) x) (cdr lis))
+;      ;; List containing x
+;      ((and (list? (car lis)) (search x (car lis))) (cons (moveXleft* x (car lis)) (cdr lis)))
+;      ;; Current element is list & next is x
+;      ((and (list? (car lis)) (and (pair? (cdr lis)) (equal? x (car (cdr lis)))))
+;       (cons (myappend (moveXleft* x (car lis)) (cons x '())) (moveXleft* x (cdr lis))))
+;      ;; Current atom & next is x
+;      ((and (pair? (cdr lis)) (equal? (car (cdr lis)) x)) (cons x (cons (car lis) (cdr (cdr lis)))))
+;      ;; x first in the list
+;      ((and (and (pair? (cdr lis)) (pair? (car (cdr lis)))) (equal? (car (car (cdr lis))) x))
+;       (myappend (cons (car lis) (cons x '())) (moveXleft* x (cdr lis)))) 
+;      (else (cons (car lis) (moveXleft* x (cdr lis)))))))

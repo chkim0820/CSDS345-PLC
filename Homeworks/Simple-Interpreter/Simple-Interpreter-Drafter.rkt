fc23101bd87d334e040ad8d54c673a056bc561aa
Chae Kim <chaekim0820@gmail.com>
2024-02-24 15:14:43 -0500

    Style changes + correct errors returned

--- Homeworks/Simple-Interpreter/Simple-Interpreter-Drafter.rkt
+++ Homeworks/Simple-Interpreter/Simple-Interpreter-Drafter.rkt
@@ -9,38 +9,14 @@
 ; MAIN FUNCTION
 ;============================================================================
 
-; This main function (should be called interpret) takes a filename, calls parser with the filename, evaluates the syntax tree
-; returned by parser, and returns the proper value. The state variable is used to represent the
-; state of the variables, and an error is returned if a variable is used before it is declared.
-; The Scheme's error function is used to return the error with the code: (error "error message").
-; The state stores variable-value pairs like this: ((x y z) (1 2 3)), where x = 1, y = 2, z = 3.
-; The state should be abstracted away from the rest of the interpreter (whatever that means).
-
-; Note about the state function vs the m-int, m-bool etc functions:
-; In this case, that means that the functions that implement the denotational semantics (m-int, m-bool, etc)
-; do not know about the structure of the state. Instead, you have generic functions that your interpreter can call 
-; to manipulate the state. (means need addBinding, removeBinding) - state shouldnt be manipulated in
-; m-int, m-bool etc
-
-
-; FOR DEBUGGING
-(define tree
-  (lambda (filename)
-    (parser filename)))
-
+; interpret takes a filename, calls parser with the filename, evaluates the syntax tree
+; returned by parser, and returns the proper value.
 (define interpret
   (lambda (filename)
     (parse-prog (parser filename) '((true false) (#t #f))))) ; the initial state is a list with 2 sublists
                                                              ; true or false stored as default
 
 ; parse-prog takes a list representing a syntax tree and returns the proper value (or error).
-; A syntax tree is represented by a list where each sublist corresponds to a statement:
-;
-; 1. variable declaration (var variable) or (var variable value)
-; 2. assignment           (= variable expression)
-; 3. return               (return expression)
-; 4. if statement         (if conditional then-statement optional-else-statement)
-; 5. while statement      (while conditional body-statement)
 (define parse-prog
   (lambda (prog state)
     (if (null? prog)
@@ -50,14 +26,6 @@
 ;============================================================================
 ; STATE FUNCTIONS (i.e. lookup, addbinding, etc.)
 ;============================================================================
-
-; The state is a list with two sublists.
-; the first sublist is a list of declared vars
-; the second sublist is a list of the corresponding values
-; For example, a state with declared vars x=1, y=2, z=3:
-; '((x y z) (1 2 3))
-; Or, an empty state, for example:
-; '(() ())
 
 ; Helper functions for abstraction
 (define get-vars    (lambda (state) (car state))) ; all vars
@@ -71,16 +39,16 @@
 (define lookup
   (lambda (var state)
     (cond
-      ((empty-state state)          'not-initialized) ; var not initialized yet; no more var
+      ((empty-state state)          (error "using before declaring")) ; var not initialized yet
       ((equal? (get-var state) var) (get-val state)) ; returns value or 'novalue depending on assignment status
       (else                         (lookup var (next-state state)))))) ; not equal; recurse down further
 
-; addbinding function
+; addbinding adds a binding to the state
 (define addbinding
   (lambda (var val state)
     (list (cons var (get-vars state)) (cons val (get-vals state))))) ; adding to state
 
-; removebinding function
+; removebinding removes a binding from the state
 (define removebinding
   (lambda (var state)
     (cond
@@ -90,7 +58,6 @@
                                                 (get-val state)
                                                 (removebinding var (next-state state)))))))
 
-; updatebinding function
 ; updatebinding adds a binding if it does not already exist; if a binding exists, it updates.
 (define updatebinding
   (lambda (var val state)
@@ -117,17 +84,19 @@
 (define arg3 cadddr)
 
 ; m-bool takes a syntax rule and a state and produces a true / false value (or an error condition).
-; this implementation of m-bool returns #t or #f
-; m-bool takes a syntax rule and a state and produces a true / false value (or an error condition).
-(define m-bool ; FIX; check if left or right operand is assignment
+(define m-bool 
   (lambda (boolexp state)
     (cond
       ((number? boolexp)             boolexp)
-      ((symbol? boolexp)            (lookup boolexp state))
+      ((symbol? boolexp)            (if (equal? (lookup boolexp state) 'novalue)
+                                        (error "used before assigned")
+                                        (lookup boolexp state)))
       ((intexp? boolexp)            (m-int boolexp state)) ; handle intexps nested in boolean expressions
       ((or (is-asgn (loperand boolexp)) ; left or right operand is an assignment stmt
            (and (roperand? boolexp) (is-asgn (roperand boolexp))))
-       (m-bool (new-stmt boolexp state) (m-state (roperand boolexp) (m-state (loperand boolexp) state))))
+       (m-bool (new-stmt (operator boolexp) (value-get (asgn-var (loperand boolexp)) (m-state (loperand boolexp) state))
+                        (value-get (asgn-var (roperand boolexp)) (m-state (roperand boolexp) (m-state (loperand boolexp) state))))
+               (m-state (roperand boolexp) (m-state (loperand boolexp) state))))
       ((eq? (operator boolexp) '&&) (and (m-bool (arg1 boolexp) state) (m-bool (arg2 boolexp) (m-state (arg1 boolexp) state))))
       ((eq? (operator boolexp) '||) (or  (m-bool (arg1 boolexp) state) (m-bool (arg2 boolexp) (m-state (arg1 boolexp) state))))
       ((eq? (operator boolexp) '!)  (not (m-bool (arg1 boolexp) (m-state (arg1 boolexp) state))))
@@ -143,7 +112,7 @@
 (define is-asgn (lambda (stmt) (if (and (list? stmt) (eq? (operator stmt) '=)) #t #f))) ; returns whether stmt is assignment or not
  
 
-; determines if an expression is an intexp, i.e. does it start with an arithmetic operator?
+; intexp? determines if an expression is an intexp, i.e. does it start with an arithmetic operator?
 (define intexp?
   (lambda (exp)
     (or (eq? (operator exp) '+)
@@ -152,7 +121,7 @@
         (eq? (operator exp) '/)
         (eq? (operator exp) '%))))
 
-; determines if an expression is a boolexp
+; boolexp? determines if an expression is a boolexp
 (define boolexp?
   (lambda (exp)
     (or (eq? (operator exp) '&&)
@@ -168,15 +137,17 @@
 (define roperand? (lambda (stmt) (if (null? (cddr stmt)) #f #t))) ; true if no 2nd operand
         
 ; m-int takes in an expression and a state and returns a numeric value
-(define m-int ; FIX; fix in case either is assignment
+(define m-int 
   (lambda (intexp state)
     (cond
       ((number? intexp)           intexp)
-      ((symbol? intexp)           (lookup intexp state)) ; lookup variable value
+      ((symbol? intexp)           (if (equal? (lookup intexp state) 'novalue)
+                                      (error "using before assigning")
+                                      (lookup intexp state))) ; lookup variable value
       ((or (is-asgn (loperand intexp)) ; left or right operand is an assignment stmt
            (and (roperand? intexp) (is-asgn (roperand intexp))))
-       (m-int (new-stmt (operator intexp) (lookup (asgn-var (loperand intexp)) (m-state (loperand intexp) state))
-                        (lookup (asgn-var (roperand intexp)) (m-state (roperand intexp) (m-state (loperand intexp) state)))) state))
+       (m-int (new-stmt (operator intexp) (value-get (asgn-var (loperand intexp)) (m-state (loperand intexp) state))
+                        (value-get (asgn-var (roperand intexp)) (m-state (roperand intexp) (m-state (loperand intexp) state)))) state))
       ((and (eq? (operator intexp) '-) (unary? intexp)) (* '-1 (m-int (loperand intexp) (m-state (loperand intexp) state))))
       ((eq? (operator intexp) '+) (+ (m-int (loperand intexp) state) (m-int (roperand intexp) (m-state (loperand intexp) state))))
       ((eq? (operator intexp) '-) (- (m-int (loperand intexp) state) (m-int (roperand intexp) (m-state (loperand intexp) state))))
@@ -188,13 +159,7 @@
 (define unary? (lambda (exp) (if (null? (cdr (cdr exp))) #t #f)))
 (define value-get (lambda (stmt state) (if (or (number? stmt) (boolean? stmt)) stmt (lookup stmt state)))) ; return value of input
 
-; state should call parse-decl, parse-asgn, etc based on which operation is necessary (i think?)
-; and then return the changed state
-; Also need to implement: 
-; looking up a binding, creating a new binding, and updating an existing binding
-
-
-; for changing state 
+; m-state changes the state
 (define m-state
   (lambda (stmt state)
     (cond
@@ -205,12 +170,9 @@
       ((eq? (operator stmt) 'return) (parse-return stmt state))
       ((eq? (operator stmt) 'if)     (parse-if stmt state))
       ((not (roperand? stmt)) (m-state (loperand stmt) state)) ; no right operand
-      (else (m-state (roperand stmt) (m-state (loperand stmt) state))))))
-      ; Else, it's m-int or m-bool with args to update
-      ; update appropriately (left -> right); can be both
-      ; redirect the appropriate terms to assignments
+      (else (m-state (roperand stmt) (m-state (loperand stmt) state)))))) ; Else, it's m-int or m-bool with args to update
 
-; check if boolean operation is done (returns bool)
+; bool-check checks if boolean operation is done (returns bool)
 (define bool-check
   (lambda (oper)
     (cond
@@ -225,7 +187,7 @@
       ((equal? oper '>=) #t)
       (else #f))))
 
-; check if arithmetic operation is done (returns int)
+; int-check checks if arithmetic operation is done (returns int)
 (define int-check
   (lambda (oper)
     (cond
@@ -236,8 +198,7 @@
       ((equal? oper '%) #t)
       (else #f))))
 
-; Parse an assignment statement.
-; takes in two statements and a state and adds the binding of the new value to the state 
+; parse-asgn takes in two statements and a state and adds the binding of the new value to the state 
 (define parse-asgn ; assuming input names valid
   (lambda (var expr state)
     (cond
@@ -253,12 +214,18 @@
                                                        (lookup (arg1 expr) (parse-asgn (arg1 expr) (arg2 expr) state))
                                                        (parse-asgn (arg1 expr) (arg2 expr) state))))))
 
-; For declaration; true if assignment with declaration, else false
-(define exp-arg (lambda (stmt) (if (null? (cdr (cdr stmt))) #f #t)))
+; exp-arg returns true if assignment with declaration, else false
+(define exp-arg (lambda (stmt) (if (null? (decl-exp stmt)) #f #t)))
 ; For converting boolean (#t/f) to true or false
 (define bool-cvt (lambda (expr) (cond ((eq? #t expr) 'true) ((eq? #f expr) 'false) (expr))))
 
-; parse-decl should take a declaration statement and add it to the state with atom 'novalue to show it hasnt been given a value
+; abstraction for exp-arg helper
+(define decl-exp
+  (lambda (stmt)
+    (cdr (cdr stmt))))
+
+; parse-decl parses declarations--
+; takes a declaration statement and adds it to the state with atom 'novalue to show it hasnt been given a value
 (define parse-decl
   (lambda (stmt state)
     (cond
@@ -283,6 +250,7 @@
 (define else-stmt (lambda (stmt) ; optional else statement
                     (if (null? (cdr (cdr (cdr stmt)))) 'no-else
                         (car (cdr (cdr (cdr stmt)))))))
+
 ; Takes in else-stmt and determine if there's a nested if statement
 (define nested-if
   (lambda (else-stmt)
@@ -291,7 +259,7 @@
       ((equal? 'if (car else-stmt)) #t)
       (else #f))))
 
-; parses a while statement
+; parse-while parses a while statement
 (define parse-while
   (lambda (stmt state)
     (cond
@@ -299,11 +267,11 @@
        (parse-while stmt (m-state (statement stmt) (m-state (condition stmt) state))))
       (else (m-state (condition stmt) state))))) ; condition is false; stop loop
 
-; abstractions for while statement
+; abstraction for while statement
 (define condition cadr)
 (define statement caddr)
 
-; Parses a return statement
+; parse-return parses a return statement
 (define parse-return
   (lambda (stmt state)
     (parse-asgn 'return (return-val stmt) (addbinding 'return 'novalue state))))

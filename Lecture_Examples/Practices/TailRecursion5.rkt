a55f0f3e6112bbe4b97aa7541e364982d738b546
Chae Kim <chaekim0820@gmail.com>
2024-02-25 19:31:25 -0500

    Update TailRecursion5.rkt

--- Lecture_Examples/Practices/TailRecursion5.rkt
+++ Lecture_Examples/Practices/TailRecursion5.rkt
@@ -11,3 +11,15 @@
       ((null? lis) 1)
       ((zero? (car lis)) (break 0))
       (else (* (car lis) (multiply-break (cdr lis) break))))))
+
+; PRACTICE:
+(define indexof
+  (lambda (x lis)
+    (call/cc (lambda (break) (indexof-break x lis break)))))
+
+(define indexof-break
+  (lambda (x lis break)
+    (cond
+      ((null? lis) (break -1))
+      ((eq? x (car lis)) 0)
+      (else (+ 1 (indexof-break x (cdr lis) break))))))
